a7a1941f4d8f57a8e55a816fc1e98163c7f4def4
Smesaz <semesile@gmail.com>
2021-11-12 22:23:34 -0500

    new advance PI #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Route exact path='/landing'>
+        <Landing/>
+      </Route>
+      <Route path='/'>
+        <Nav/>
+      </Route>
+      <Route path='/home'>
+        <Home/>
+      </Route>
+      <Route path='/details'>
+        <Details/>
+      </Route>
+      <Route path='/newbreed'>
+        <NewBreed/>
+      </Route>
     </div>
   );
 }
---
+++ client/src/reducer/index.js
@@ -0,0 +1,13 @@
+const initialState=[];
+
+const breeds_reucer = (state=initialState, action) =>{
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+export default breeds_reucer;
